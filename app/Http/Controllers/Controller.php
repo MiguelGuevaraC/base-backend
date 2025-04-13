cdd1307030eeeb8f03f8c9fc141ea2f231ec9c76
MiguelGuevaraC <mguevaracaj@unprg.edu.pe>
2025-04-13 04:33:51 -0500

    feat: swagger documentación

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Traits\Filterable;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+
+/**
+ *  @OA\Info(
+ *      title="API's BASE BACKEND",
+ *      version="1.0.0",
+ *      description="API's for base",
+ * ),
+ *   @OA\SecurityScheme(
+ *     securityScheme="bearerAuth",
+ *     in="header",
+ *     name="Authorization",
+ *     type="http",
+ *     scheme="bearer",
+ *     bearerFormat="JWT",
+ * ),
+
+ */
 abstract class Controller
 {
-    //
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests, Filterable;
+
 }
